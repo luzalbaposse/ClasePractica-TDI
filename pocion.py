97ae53d7af4f7f38433de9e01bf981cbd36b120f
Luz Alba Posse <luzalbaposse@gmail.com>
2023-06-06 08:02:13 -0300

    Update pocion.py

--- pocion.py
+++ pocion.py
@@ -3,16 +3,41 @@ from typing import List
 
 class Pocion:
     ''' Representa una poción del universo de Harry Potter.
-        Una poción p tiene atributos:
+        Atributos:
             - p.nombre (str)
             - p.ingredientes_conocidos (List[str])
             - p.efecto (str)
             - p.caracteristicas (str)
             - p.nivel_dificultad (str)
-        y métodos:
+        Métodos:
             - representación como string
-            - menor que: <
+            -  dos pociones p1 y p2 puedan compararse por menor de forma tal que
+                p1 < p2 sea verdadero si el nombre de p1 es menor al de p2 de acuerdo
+                al orden lexicográfico
     '''
+    # Defino los atributos
+    nombre:str
+    ingredientes_conocidos:List[str]
+    efecto:str
+    caracteristicas:str
+    nivel_dificultad:str
+
+    #Defino Métodos
+    def __str__(self)-> str: # Representación como string
+        '''
+        Devuelve una representación como string de la poción.
+        Requiere: nada.
+        '''
+        return f'Poción {self.nombre}'
+    
+    def __lt__ (self, other:Pocion)-> bool: # Comparación por menor de dos pociones en base a su nombre (orden lexicográfico)
+        '''
+        Devuelve True si la poción self es menor que la poción other.
+        Requiere: nada.
+        '''
+        return self.nombre < other.nombre
+    
+
     def __init__(self, n:str, ic:List[str], e:str, c:str, nd:str):
         '''
         Inicializa una poción con nombre n, ingredientes secretos ic,
